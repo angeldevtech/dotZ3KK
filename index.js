a3d604881790c6509ff76cef3e6b7b1b52122d11
Angel Dev <57205952+angeldevtech@users.noreply.github.com>
2020-06-18 16:05:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const PREFIX = '!';
+
 client.once('ready', () => {
     console.log('dotZ3KK is online!');
+});
+
+client.on("message", message => {
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'cpp':
+            message.channel.send("¿Qué fue loco?¿Por qué eres un CPP?");
+            break;
+        case 'version':
+            message.channel.send("Esta es la version 1.0.0, ojalá me mejoren UwU");
+            break;
+        case 'author':
+            message.channel.send("@dotGlass");
+            break;
+    }
+
 });
 
 client.login(process.env.token);
