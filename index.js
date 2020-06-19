bcb227f145adb9179a83ad6a57c1c6701829ee5e
Angel Dev <57205952+angeldevtech@users.noreply.github.com>
2020-06-19 09:20:30 -0500

    Try to fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.once('ready', () => {
 
 client.on("message", message => {
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    //if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(PREFIX.length).split(/ +/);
     const command = args.shift().toLowerCase();
