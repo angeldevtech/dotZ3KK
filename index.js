c750662a95e5bb1b8c9f55e531eab278f3ee1a17
Angel Dev <57205952+angeldevtech@users.noreply.github.com>
2021-10-06 20:39:28 -0500

    Audio Stream v1.0
    hope

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Discord = require('discord.js');
+const {AudioManager} = require('discordaudio');
 
-const client = new Discord.Client();
+const client = new Discord.Client({intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MESSAGES, discord.Intents.FLAGS.GUILD_VOICE_STATES]});
 
 const PREFIX = '!';
+
+const connections = new Map();
+
+const audioManager = new AudioManager();
 
 client.once('ready', () => {
     console.log('dotZ3KK is online!');
@@ -30,6 +35,54 @@ client.on("message", message => {
         "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"];
 
     switch(command){
+        case 'play':
+            if(!message.member.voice.channel && !message.guild.me.voice.channel) return message.channel.send({content: `Please join a voice channel in order to play a song!`});
+            if(!args[1]) return message.channel.send({content: `Please provide a song`});
+            const uvc = message.member.voice.channel || message.guild.me.voice.channel;
+            audioManager.play(uvc, args[1], {
+                quality: 'high',
+                audiotype: 'arbitrary',
+                volume: 10
+            }).then(queue => {
+                connections.set(uvc.id, uvc);
+                if(queue === false) message.channel.send({content: `Your song is now playing!`});
+                else message.channel.send({content: `Your song has been added to the queue!`});
+            }).catch(err => {
+                console.log(err);
+                message.channel.send({content: `There was an error while trying to connect to the voice channel!`});
+            });
+            break;
+        case 'skip':
+            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
+            audioManager.skip(vc).then(() => message.channel.send({content: `Successfully skipped the song!`})).catch(err => {
+                console.log(err);
+                message.channel.send({content: `There was an error while skipping the song!`});
+            });
+            break;
+        case 'stop':
+            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
+            audioManager.stop(vc);
+            message.channel.send({content: `Player successfully stopped!`});            
+            break;
+        case 'queue':
+            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
+            const queue = audioManager.queue(vc).reduce((text, song, index) => {
+                if(song.title) text += `**[${index + 1}]** ${song.title}`;
+                else text += `**[${index + 1}]** ${song.url}`;
+                return text;
+            }, `__**QUEUE**__`);
+            const queueEmbed = new discord.MessageEmbed()
+            .setColor(`BLURPLE`)
+            .setTitle(`Queue`)
+            .setDescription(queue);
+            message.channel.send({embeds: [queueEmbed]});
+            break;
+        case 'volume':
+            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
+            if(!args[1]) return message.channel.send({content: `Please provide the volume`});
+            if(Number(args[1] < 1 || Number(args[1]) > 10)) return message.channel.send({content: `Please provide a volume between 1-10`});
+            audioManager.volume(vc, Number(args[1]));
+            break;
         case 'cpp':
             if( args[0] != null ){
                 var userMention = message.mentions.users.first();
