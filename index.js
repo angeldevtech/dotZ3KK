709fa26a636eb581788f520a6880f61fb61a7359
Angel Dev <57205952+angeldevtech@users.noreply.github.com>
2021-12-16 13:24:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,54 +37,6 @@ client.on("messageCreate", message => {
     const vc = connections.get(message.guild.me.voice.channel?.id);
 
     switch(command){
-        case 'play':
-            if(!message.member.voice.channel && !message.guild.me.voice.channel) return message.channel.send({content: `Please join a voice channel in order to play a song!`});
-            if(!args[1]) return message.channel.send({content: `Please provide a song`});
-            const uvc = message.member.voice.channel || message.guild.me.voice.channel;
-            audioManager.play(uvc, args[1], {
-                quality: 'high',
-                audiotype: 'arbitrary',
-                volume: 10
-            }).then(queue => {
-                connections.set(uvc.id, uvc);
-                if(queue === false) message.channel.send({content: `Your song is now playing!`});
-                else message.channel.send({content: `Your song has been added to the queue!`});
-            }).catch(err => {
-                console.log(err);
-                message.channel.send({content: `There was an error while trying to connect to the voice channel!`});
-            });
-            break;
-        case 'skip':
-            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
-            audioManager.skip(vc).then(() => message.channel.send({content: `Successfully skipped the song!`})).catch(err => {
-                console.log(err);
-                message.channel.send({content: `There was an error while skipping the song!`});
-            });
-            break;
-        case 'stop':
-            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
-            audioManager.stop(vc);
-            message.channel.send({content: `Player successfully stopped!`});            
-            break;
-        case 'queue':
-            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
-            const queue = audioManager.queue(vc).reduce((text, song, index) => {
-                if(song.title) text += `**[${index + 1}]** ${song.title}`;
-                else text += `**[${index + 1}]** ${song.url}`;
-                return text;
-            }, `__**QUEUE**__`);
-            const queueEmbed = new discord.MessageEmbed()
-            .setColor(`BLURPLE`)
-            .setTitle(`Queue`)
-            .setDescription(queue);
-            message.channel.send({embeds: [queueEmbed]});
-            break;
-        case 'volume':
-            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
-            if(!args[1]) return message.channel.send({content: `Please provide the volume`});
-            if(Number(args[1] < 1 || Number(args[1]) > 10)) return message.channel.send({content: `Please provide a volume between 1-10`});
-            audioManager.volume(vc, Number(args[1]));
-            break;
         case 'cpp':
             if( args[0] != null ){
                 var userMention = message.mentions.users.first();
@@ -155,91 +107,11 @@ client.on("messageCreate", message => {
                 message.channel.send("https://k62.kn3.net/taringa/E/6/8/D/6/7/FernandoTedesco/550x366_AC6.jpg");
             }
             break;
-            
+        case 'then':
+            const textThen = message.content.slice(6);
+            message.channel.send("https://textoverimage.moesif.com/image?image_url=https%3A%2F%2Fi.imgur.com%2FZdiRBOg.png&text=text&"+textThen+"_size=128&y_align=bottom&x_align=center")    
     }
 
 });
 
 client.login(process.env.token);
-/*
-import { AudioManager } from 'discordaudio';
-import { Client, Intents, MessageEmbed } from 'discord.js';
-
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]});
-
-const config = {
-    token: 'Your-Secret-Token',
-    prefix: '!'
-};
-
-const connections = new Map();
-
-const audioManager = new AudioManager();
-
-client.once('ready', () => console.log(`${client.user.username} is online!`));
-
-client.on('messageCreate', message => {
-    if(message.author.bot || message.channel.type === `DM`) return;
-    
-    if(!message.content.startsWith(config.prefix)) return;
-    
-    let args = message.content.substring(config.prefix.length).split(" ");
-    
-    const vc = connections.get(message.guild.me.voice.channel?.id);
-    
-    switch(args[0].toLowerCase()){
-        case 'play':
-            if(!message.member.voice.channel && !message.guild.me.voice.channel) return message.channel.send({content: `Please join a voice channel in order to play a song!`});
-            if(!args[1]) return message.channel.send({content: `Please provide a song`});
-            const uvc = message.member.voice.channel || message.guild.me.voice.channel;
-            audioManager.play(uvc, args[1], {
-                quality: 'high',
-                audiotype: 'arbitrary',
-                volume: 10
-            }).then(queue => {
-                connections.set(uvc.id, uvc);
-                if(queue === false) message.channel.send({content: `Your song is now playing!`});
-                else message.channel.send({content: `Your song has been added to the queue!`});
-            }).catch(err => {
-                console.log(err);
-                message.channel.send({content: `There was an error while trying to connect to the voice channel!`});
-            });
-            break;
-        case 'skip':
-            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
-            audioManager.skip(vc).then(() => message.channel.send({content: `Successfully skipped the song!`})).catch(err => {
-                console.log(err);
-                message.channel.send({content: `There was an error while skipping the song!`});
-            });
-            break;
-        case 'stop':
-            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
-            audioManager.stop(vc);
-            message.channel.send({content: `Player successfully stopped!`});            
-            break;
-        case 'queue':
-            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
-            const queue = audioManager.queue(vc).reduce((text, song, index) => {
-                if(song.title) text += `**[${index + 1}]** ${song.title}`;
-                else text += `**[${index + 1}]** ${song.url}`;
-                return text;
-            }, `__**QUEUE**__`);
-            const queueEmbed = new MessageEmbed()
-            .setColor(`BLURPLE`)
-            .setTitle(`Queue`)
-            .setDescription(queue);
-            message.channel.send({embeds: [queueEmbed]});
-            break;
-        case 'volume':
-            if(!vc) return message.channel.send({content: `There is currently nothing playing!`});
-            if(!args[1]) return message.channel.send({content: `Please provide the volume`});
-            if(Number(args[1] < 1 || Number(args[1]) > 10)) return message.channel.send({content: `Please provide a volume between 1-10`});
-            audioManager.volume(vc, Number(args[1]));
-            break;
-        case 'tryme':
-            message.channel.send(`working`)
-    }
-});
-
-client.login(process.env.token);
-*/
